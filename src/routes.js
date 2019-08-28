28ea9bfefecda3a312188340704585847dc2c923
naeliofreires <naeliofreires@gmail.com>
2019-08-27 22:35:30 -0300

    aplicando paginacao / listando agenda do prestador

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ import AppointmentController from './app/controllers/AppointmentController';
 
 // middleware
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(configMulter);
@@ -28,6 +29,8 @@ routes.put('/users', UserController.update);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
